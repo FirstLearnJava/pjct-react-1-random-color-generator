ac6a1ee2ba587179d97989e04e9b1f5080c019da
MattHere <matthiasfloimayr@gmail.com>
2023-05-03 19:02:56 +0200

    Finished project without preflight errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
-import logo from './logo.svg';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 export default function App() {
+  function getRandomColor() {
+    const someColor = randomColor({});
+    return someColor;
+  }
+  const [backgroundColor, setBackgroundColor] = useState(getRandomColor());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ backgroundColor }}>
+        <p>background color {backgroundColor}</p>
+        <br />
+        <button onClick={() => setBackgroundColor(getRandomColor())}>
+          Generate
+        </button>
+      </div>
     </div>
   );
 }
